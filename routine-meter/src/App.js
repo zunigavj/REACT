33117e4e58b0da3814f1e974ed14a9f14bc4c818
jason.zunigav@gmail.com <ZUNIGAVJ@bccr.fi.cr>
2021-12-04 23:39:57 -0600

    Se agrega titulo, slogan, iconos y estructura general

--- routine-meter/src/App.js
+++ routine-meter/src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import logo from './stopwatchLogo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+	<div>
+	  <img src={logo}  alt="logo" />
+	  <p>Routine Meter</p>
+	  <p>Cause time matter!</p>
+	</div>
+	<div className="ActivityContainer">
+	  <input class="txtAgregarActividad" type="text" placeholder="Activity"/>
+	  <input class="btnAgregarActividad" type="button" value="Add"/>
+	  <ul>
+	    <li>Activity one</li>
+	    <li>Activity two</li>
+	    <li>Activity three</li>
+	    <li>Activity four</li>
+	  </ul>
+	  <div>Total routine time: </div>
+	</div>
       </header>
+      <footer>
+	  <p>Jason Arturo Zúñiga</p> 
+	  <p>jason.zunigavj@gmail.com</p> 
+      </footer>
     </div>
   );
 }
