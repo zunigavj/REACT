95ef6418b0d4a33c385c58579103e3ec7fd1e8df
jason.zunigav@gmail.com <ZUNIGAVJ@bccr.fi.cr>
2021-12-04 23:53:42 -0600

    Se ajusta espaciado

--- routine-meter/src/App.js
+++ routine-meter/src/App.js
@@ -4,28 +4,28 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-	<div>
-	  <img src={logo}  alt="logo" />
-	  <p>Routine Meter</p>
-	  <p>Cause time matter!</p>
-	</div>
-	<div className="ActivityContainer">
-	  <input class="txtAgregarActividad" type="text" placeholder="Activity"/>
-	  <input class="btnAgregarActividad" type="button" value="Add"/>
-	  <ul>
-	    <li>Activity one</li>
-	    <li>Activity two</li>
-	    <li>Activity three</li>
-	    <li>Activity four</li>
-	  </ul>
-	  <div>Total routine time: </div>
-	</div>
-      </header>
-      <footer>
-	  <p>Jason Arturo Zúñiga</p> 
-	  <p>jason.zunigavj@gmail.com</p> 
-      </footer>
+    <header className="App-header">
+		<div>
+			<img src={logo}  alt="logo" />
+			<p>Routine Meter</p>
+			<p>Cause time matter!</p>
+		</div>
+		<div className="ActivityContainer">
+			<input class="txtAgregarActividad" type="text" placeholder="Activity"/>
+			<input class="btnAgregarActividad" type="button" value="Add"/>
+			<ul>
+				<li>Activity one</li>
+				<li>Activity two</li>
+				<li>Activity three</li>
+				<li>Activity four</li>
+			</ul>
+			<div>Total routine time: </div>
+		</div>
+	</header>
+	<footer>
+		<p>Jason Arturo Zúñiga</p> 
+		<p>jason.zunigavj@gmail.com</p> 
+    </footer>
     </div>
   );
 }
